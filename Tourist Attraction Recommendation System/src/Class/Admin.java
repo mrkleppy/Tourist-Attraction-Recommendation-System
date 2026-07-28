11e5e04745ce9a38eee3b9b6d3358f588c77dad5
Confilcted <94037746+Confilcted@users.noreply.github.com>
2026-07-28 22:16:01 +0800

    New Classes
    4 new classes

---
+++ Tourist Attraction Recommendation System/src/Class/Admin.java
@@ -0,0 +1,11 @@
+public class Admin extends User {
+
+    public Admin() {
+        super("", "");
+    }
+
+    public Admin(String username, String password) {
+        super(username, password);
+    }
+
+}
