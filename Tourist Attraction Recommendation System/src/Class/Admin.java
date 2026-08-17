c31389872e4c8725b586d5f5f781b8f433237286
Confilcted <94037746+Confilcted@users.noreply.github.com>
2026-08-17 13:36:09 +0800

    Update Admin.java
    added remove city method

--- Tourist Attraction Recommendation System/src/Class/Admin.java
+++ Tourist Attraction Recommendation System/src/Class/Admin.java
@@ -4,32 +4,58 @@ import java.util.List;
 import java.util.Objects;
 
 public class Admin extends User {
-    public Admin() {
-        super("", "", "admin");
-    }
-
+ 
     public Admin(String username, String password) {
-        super(username, password, "admin");
+        super(username, password);
     }
-
-    @Override
-    public String toString() {
-        return super.toString();
+ 
+    /**
+     * Removes the given city from the state's list of cities, if present.
+     * Returns true if a city was removed, false if it wasn't found.
+     */
+    public boolean removeCity(State state, City city) {
+        return removeCity(state, city.getName());
     }
-    
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        
-        if (obj == null || this.getClass() != obj.getClass()) {
+ 
+    /**
+     * Removes the first city with a matching name (case-insensitive)
+     * from the state's list of cities.
+     */
+    public boolean removeCity(State state, String cityName) {
+        City[] cities = state.getCities();
+        if (cities == null || cities.length == 0) {
             return false;
         }
-        
-        Admin admin = (Admin)obj;
-        return this.getUsername().equals(admin.getUsername());
+ 
+        int indexToRemove = -1;
+        for (int i = 0; i < cities.length; i++) {
+            if (cities[i].getName().equalsIgnoreCase(cityName)) {
+                indexToRemove = i;
+                break;
+            }
+        }
+ 
+        if (indexToRemove == -1) {
+            return false;
+        }
+ 
+        City[] updated = new City[cities.length - 1];
+        int j = 0;
+        for (int i = 0; i < cities.length; i++) {
+            if (i != indexToRemove) {
+                updated[j++] = cities[i];
+            }
+        }
+ 
+        state.setCities(updated);
+        return true;
     }
+ 
+    @Override
+    public String getRole() {
+        return "Admin";
+    }
+}
     
     @Override
     public int hashCode() {
